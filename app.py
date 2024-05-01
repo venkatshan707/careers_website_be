5e00737c9102209d9b679d208c3636f1cecb73b0
VENKATESAN S <venkatshan707@gmail.com>
2024-05-01 11:14:14 +0000

    Sending Dynamic data to HTML  from python list

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template  # from flask Module importing Flask class
+from flask import Flask, jsonify, render_template  # from flask Module importing Flask class
 
 app = Flask(__name__)  # our app is just a object of the class Flask
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Chennai, India',
+}, {
+    'id': 2,
+    'title': 'Data Scienist',
+    'location': 'Bangalore, India',
+    'salary': 'Rs. 20 LPA'
+}, {
+    'id': 3,
+    'title': 'Machine learning Engineer',
+    'location': 'Chennai, India',
+    'salary': 'Rs. 15 LPA'
+}, {
+    'id': 4,
+    'title': 'Tableau Developer',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 10 LPA'
+}]
 
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+
+  #Returning a HTML Endpoint
+  return render_template(
+      'home.html', jobs=JOBS, company_name="Thedal"
+  )  # Sending jobs (dynamic content) to home.html using render_template as a argument
+
+
+@app.route('/api/jobs')
+def jobs():
+  #Returning a json Endpoint
+  return jsonify(JOBS)  # Webserver Returning jobs as a json
 
 
 if __name__ == '__main__':
