33c8bea598b2b6e89d186453485083026676516d
venkatshan <venkatshan707@gmail.com>
2024-05-27 07:48:34 +0530

    Converted jobs as JSON Serialization

--- app.py
+++ app.py
@@ -1,32 +1,19 @@
 from flask import Flask, jsonify, render_template  # from flask Module importing Flask class
+from database import load_jobs_from_db # from database
+from sqlalchemy import  text
+
 
 app = Flask(__name__)  # our app is just a object of the class Flask
 
-JOBS = [{
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Chennai, India',
-}, {
-    'id': 2,
-    'title': 'Data Scienist',
-    'location': 'Bangalore, India',
-    'salary': 'Rs. 20 LPA'
-}, {
-    'id': 3,
-    'title': 'Machine learning Engineer',
-    'location': 'Chennai, India',
-    'salary': 'Rs. 15 LPA'
-}, {
-    'id': 4,
-    'title': 'Tableau Developer',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 10 LPA'
-}]
+
+
+
+
 
 
 @app.route('/')
 def home():
-
+  JOBS=load_jobs_from_db()
   #Returning a HTML Endpoint
   return render_template(
       'home.html', jobs=JOBS, company_name="Thedal"
@@ -35,6 +22,7 @@ def home():
 
 @app.route('/api/jobs')
 def jobs():
+  JOBS=load_jobs_from_db()
   #Returning a json Endpoint
   return jsonify(JOBS)  # Webserver Returning jobs as a json
 
--- database.py
+++ database.py
@@ -26,8 +26,8 @@ cursor = conn.cursor()
 cursor.execute(SQL_QUERY)
 
 records = cursor.fetchall()
-for r in records:
-            print(f"{r.id}\t{r.title}\t{r.location}\t{r.currency} {r.salary}")
+# for r in records:
+#             print(f"{r.id}\t{r.title}\t{r.location}\t{r.currency} {r.salary}")
 
 
 
@@ -40,7 +40,8 @@ for r in records:
 engine = create_engine(f"mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver=ODBC+Driver+18+for+SQL+Server")
 
 
-with engine.connect() as conn: # Giving name to the connection established by the engine as conn
+def load_jobs_from_db():
+  with engine.connect() as conn: # Giving name to the connection established by the engine as conn
         # Then is connection used to execute SQL commands 
         print ("\n","Interaction with SQL Engine".center(40, '#'),"\n") 
         result = conn.execute(text('SELECT * FROM dbo.jobs'))
@@ -52,16 +53,16 @@ with engine.connect() as conn: # Giving name to the connection established by th
         #Actually our database have column names and values to it. we can think of like key : value pair 
         #we can achieve key value pair, by typecasting each row as dict  
         
-        result_as_dict =result.mappings().all()# Converting as dictionary
-
-        """The mappings() method of the CursorResult object returns a RowProxy object, which is a row-level collection of 
-        result columns. The all() method then converts this RowProxy object into a list of dictionaries, where each dictionary
-         represents a row in the result set."""
-
-        print(type(result_as_dict))#the result_as_dict variable will be a list of dictionaries, where each dictionary represents a row in the users table.
+        jobs_as_list_of_dict =  [dict(row) for row in result.mappings().all()]# Converting as dictionary
+        
+        #The expression [dict(row) for row in result.mappings().all()] is a list comprehension that converts 
+        #a CursorResult object (returned by a SQLAlchemy database query) into a list of dictionaries, which is particularly useful for JSON serialization
+        
+        print("Type :  ",type(jobs_as_list_of_dict))#the result_as_dict variable will be a list of dictionaries, where each dictionary represents a row in the users table.
         
         
-        for r in result_as_dict:        
-            print(f"{r.id}\t{r.title}\t{r.location}\t{r.currency} {r.salary}")
-# when we comming out of 'with' our connection will be automatically closed 
-
+        # for r in jobs_as_list_of_dict:        
+        #     print(f"{r.id}\t{r.title}\t{r.location}\t{r.currency} {r.salary}")
+        return jobs_as_list_of_dict  
+      # # when we comming out of 'with' our connection will be automatically closed 
+        
